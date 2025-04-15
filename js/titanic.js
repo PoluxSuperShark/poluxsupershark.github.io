b8d118e477d4c7e74033d12b37b5a22eddc64519
PoluxSuperShark <poluxcity@gmail.com>
2025-04-15 03:44:27 +0200

    Added CSS and edited Titanic JS file.

--- js/titanic.js
+++ js/titanic.js
@@ -1,8 +1,16 @@
+/**
+ * Fonction pour ouvrir l'URL du site de
+ * http://abtitanic.free.fr
+ * Fichier depuis : poluxsupershark.github.io/pages/about/titanic.html
+ */
 function openLinkTitanic() {
+    // Définir l'URL dans une variable
     const url = 'http://abtitanic.free.fr';
+    // Alerter l'utilisateur qu'il quitte le site
     const confirmation = confirm(
         'Vous allez quitter le site pour :\n' + url + '\nVoulez-vous continuer ?'
     );
+    // Si le prompt a été confirmé
     if (confirmation) {
         window.open(url, '_blank');
     }
